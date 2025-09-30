c7fa7a374154bbc18f5579271c6cef0b44500c05
Eryk Kryszewski <eric9972@o2.pl>
2025-09-30 09:54:31 +0200

    stuff done - backend progress

--- acf/blocks/two-box-promo.php
+++ acf/blocks/two-box-promo.php
@@ -1,17 +1,34 @@
 <?php
-$url='http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
-$section_id=get_field('section_id');
-$title=get_field('title');
-$subtitle=get_field('subtitle');
-$text=get_field('text');
+$url = "http://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+$section_id = get_field("section_id");
+
+$left_column_content = get_field("left_column_content");
+$right_up_content = get_field("right_up_content");
+$left_down_column = get_field("left_down_column");
+$right_down_column = $left_down_column;
+$button = get_field("button");
 ?>
+
 <div class="two-box-promo">
-	<?php if(!empty($section_id)):?><div class="section-id" id="<?php echo esc_html($section_id);?>"></div><?php endif;?>
-	<div class="container">
-		<div class="two-box-promo__wrapper">
-			<?php if(!empty($title)):?><h2 class="two-box-promo__title"><?php echo apply_filters('the_title',$title);?></h2><?php endif;?>
-			<?php if(!empty($subtitle)):?><h3 class="two-box-promo__subtitle"><?php echo apply_filters('the_title',$subtitle);?></h3><?php endif;?>
-			<?php if(!empty($text)):?><?php echo apply_filters('acf_the_content',str_replace('&nbsp;',' ',$text));?><?php endif;?>
-		</div>
-	</div>
+    <?php if (!empty($section_id)): ?>
+    <div class="section-id" id="<?php echo esc_html($section_id); ?>"></div>
+    <?php endif; ?>
+    <div class="container">
+        <div class="two-box-promo__wrapper">
+            <?php if (!empty($left_column_content)): ?>
+            <div class="two-box-promo__column two-box-promo__column--left"><?php echo apply_filters('the_title', $left_column_title); ?></div>
+            <?php endif; ?> <?php if (!empty($right_up_content)): ?>
+            <div class="two-box-promo__column two-box-promo__column--right">
+                <div class="two-box-promo__info">
+                    <?php foreach ($right_up_content as $key => $item): ?>
+                    <div class="two-box-promo__item">
+                        <div class="two-box-promo__days"><?php echo apply_filters('the_title', $item['days']); ?></div>
+                        <div class="two-box-promo__discount"><?php echo apply_filters('the_title', $item['discount']); ?></div>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+            <?php endif; ?>
+        </div>
+    </div>
 </div>
--- single.php
+++ single.php
@@ -12,18 +12,67 @@ global $post;
 
 $post = get_post();
 $page_id = $post->ID;
+
+$left_column_content = get_field("left_column_content", $page_id);
+$right_up_content = get_field("right_up_content", $page_id);
+$left_down_column = get_field("left_down_column", $page_id);
+$right_down_content = $left_down_column;
+$button = get_field("button", $page_id);
 ?>
 
 <main id="main" class="main main--subpage">
     <?php if (have_posts()): ?> <?php while (have_posts()): the_post(); ?> <?php if (get_post_type() == 'promocje'): ?>
 
     <!-- CPT -->
-    <?php if (!empty(get_post_thumbnail_id($post->ID))): ?>
-    <div class="single-blog-post__image"><?php echo wp_get_attachment_image(get_post_thumbnail_id($post->ID), 'full', '', [ 'class' => 'object-fit-cover', ]); ?></div>
-    <?php endif; ?>
-    <h2><?php the_title(); ?></h2>
-    <p><?php the_content(); ?></p>
-    <div class="single-course"><?php the_content(); ?></div>
+    <div class="promotion">
+        <div class="container">
+            <div class="promotion__wrapper">
+                <?php if (!empty(get_post_thumbnail_id($post->ID))): ?>
+                <div class="promotion__image">
+                    <?php echo wp_get_attachment_image(get_post_thumbnail_id($post->ID), 'full', '', [ 'class' => 'object-fit-cover', ]); ?>
+                </div>
+                <?php endif; ?>
+                <div class="promotion__content">
+                    <h1><?php the_title(); ?></h1>
+                    <p><?php the_content(); ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="two-box-promo">
+        <?php if (!empty($section_id)): ?>
+        <div class="section-id" id="<?php echo esc_html($section_id); ?>"></div>
+        <?php endif; ?>
+        <div class="container">
+            <div class="two-box-promo__wrapper">
+                <?php if (!empty($left_column_content)): ?>
+                <div class="two-box-promo__column two-box-promo__column--left">
+                    <?php echo apply_filters('the_title', $left_column_content,); ?>
+                </div>
+                <?php endif; ?> <?php if (!empty($right_up_content)): ?>
+                <div class="two-box-promo__column two-box-promo__column--right">
+                    <div class="two-box-promo__info">
+                        <?php foreach ($right_up_content as $key => $item): ?>
+                        <div class="two-box-promo__item">
+                            <div class="two-box-promo__days"><?php echo apply_filters('the_title', $item['days']); ?></div>
+                            <div class="two-box-promo__discount">
+                                <?php echo apply_filters('the_title', $item['discount'],); ?>
+                            </div>
+                        </div>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="two-box-promo__cta">
+                        <div class="two-box-promo__encourage-text"><?php echo apply_filters('the_title', $right_down_content); ?></div>
+                        <?php if (!empty($button)): ?>
+                        <a href="<?php echo esc_html($button['url']); ?>" class="button two-box-promo__button"><?php echo esc_html($button['title']); ?></a>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
 
     <?php else: ?>
 
@@ -32,9 +81,7 @@ $page_id = $post->ID;
         <div class="subpage-hero__background subpage-hero__background--plain"></div>
         <div class="container">
             <div class="subpage-hero__wrapper">
-                <h1 class="subpage-hero__title subpage-hero__title--white">
-                    <?php echo apply_filters('the_title', 'Blog',); ?>
-                </h1>
+                <h1 class="subpage-hero__title subpage-hero__title--white"><?php echo apply_filters('the_title', 'Blog'); ?></h1>
             </div>
         </div>
     </div>
@@ -44,7 +91,9 @@ $page_id = $post->ID;
                 <div class="col-12 col-lg-10 offset-lg-1">
                     <div class="single-blog-post__content">
                         <?php if (!empty(get_post_thumbnail_id($post->ID))): ?>
-                        <div class="single-blog-post__image"><?php echo wp_get_attachment_image(get_post_thumbnail_id($post->ID), 'full', '', [ 'class' => 'object-fit-cover', ]); ?></div>
+                        <div class="single-blog-post__image">
+                            <?php echo wp_get_attachment_image(get_post_thumbnail_id($post->ID), 'full', '', ['class' => 'object-fit-cover'],); ?>
+                        </div>
                         <?php endif; ?>
                         <h2><?php the_title(); ?></h2>
                         <p><?php the_content(); ?></p>
