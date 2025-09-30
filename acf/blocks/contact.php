a197e680c7f04fd2c07cbfc269b9f2d9ba3952bd
Eryk Kryszewski <eric9972@o2.pl>
2025-09-30 19:56:49 +0200

    backend progress

--- acf/blocks/contact.php
+++ acf/blocks/contact.php
@@ -1,73 +1,53 @@
 <?php
-$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-$background = get_field('background');
-$section_id = get_field('section_id');
+$url = "http://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+$background = get_field("background");
+$section_id = get_field("section_id");
 
-$text = get_field('text');
-$form_id = get_field('form_id');
-$image = get_field('image');
+$content = get_field("content");
+$button = get_field("button");
+$map = get_field("map");
 
-$global_phone_number = get_field('global_phone_number', 'options');
-$global_email = get_field('global_email', 'options');
-$global_social_media = get_field('global_social_media', 'options');
-$global_opening_hours = get_field('global_opening_hours', 'options');
+$allowed_stuff = [
+    "iframe" => [
+        "src" => true,
+        "width" => true,
+        "height" => true,
+        "frameborder" => true,
+        "style" => true,
+        "allow" => true,
+        "allowfullscreen" => true,
+        "loading" => true,
+        "referrerpolicy" => true,
+    ],
+];
 ?>
 
-<?php if (!empty($form_id)): ?>
-<div class="contact <?php if ($background == 'true') {
-  echo 'contact--background';
-} ?>">
-  <?php if (!empty($section_id)): ?>
-  <div class="section-id" id="<?php echo esc_html($section_id); ?>"></div>
-  <?php endif; ?>
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6 col-xl-5">
-        <div class="contact__details">
-          <?php if (!empty($image)): ?>
-          <div class="contact__image">
-            <?php echo wp_get_attachment_image($image, 'full', '', ['class' => 'object-fit-cover']); ?>
-            <div class="contact__image-decorator"></div>
-          </div>
-          <?php endif; ?>
-          <?php if (!empty($text)): ?>
-          <?php echo apply_filters('acf_the_content', str_replace('&nbsp;', ' ', $text)); ?>
-          <?php endif; ?>
-          <?php if (!empty($global_phone_number)): ?>
-          <a class="contact__phone ercodingtheme-phone-number"
-            href="tel:<?php echo esc_html($global_phone_number); ?>">Tel:
-            <?php echo esc_html($global_phone_number); ?></a>
-          <?php endif; ?>
-          <?php if (!empty($global_email)): ?>
-          <a class="contact__email" href="mailto:<?php echo esc_html($global_email); ?>">Mail:
-            <?php echo esc_html($global_email); ?></a>
-          <?php endif; ?>
-          <!-- <?php if (!empty($global_opening_hours)): ?>
-              <h4 class="contact__subtitle"><?php esc_html_e('Godziny otwarcia:', 'ercodingtheme'); ?></h4>
-              <div class="opening-hours contact__opening-hours">
-                <?php echo apply_filters('acf_the_content', $global_opening_hours); ?>
-              </div>
-            <?php endif; ?> -->
-          <h4 class="contact__subtitle"><?php esc_html_e('Obserwuj nas:', 'ercodingtheme'); ?></h4>
-          <?php if (!empty($global_social_media)): ?>
-          <div class="social-media contact__social-media">
-            <?php foreach ($global_social_media as $key => $item): ?>
-            <a href="<?php echo esc_url_raw($item['link']); ?>" target="_blank">
-              <?php if (!empty($item['icon'])): ?>
-              <?php echo wp_get_attachment_image($item['icon'], 'full', '', ['class' => 'object-fit-contain']); ?>
-              <?php endif; ?>
-            </a>
-            <?php endforeach; ?>
-          </div>
-          <?php endif; ?>
+<?php if (!empty($map)): ?>
+<div class="contact <?php if ($background == 'true') { echo 'contact--background'; } ?>"
+>
+    <?php if (!empty($section_id)): ?>
+    <div class="section-id" id="<?php echo esc_html($section_id); ?>"></div>
+    <?php endif; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-xl-4">
+                <div class="contact__details">
+                    <?php if (!empty($content)): ?>
+                    <div class="contact__content"><?php echo apply_filters('the_title', $content); ?></div>
+                    <?php endif; ?> <?php if (!empty($button)): ?>
+                    <a class="contact__button button" href="<?php echo esc_html($button['url'],); ?>"
+                    >
+                        <?php echo esc_html($button['title']); ?>
+                    </a>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="col-lg-6 col-xl-8">
+                <div class="contact__map">
+                    <div class="contact__iframe"><?php echo wp_kses($map, $allowed_stuff); ?></div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="col-lg-6 col-xl-7">
-        <div class="contact__form ercoding-form">
-          <?php echo gravity_form($form_id, false, false, false, '', false, 11); ?>
-        </div>
-      </div>
     </div>
-  </div>
 </div>
 <?php endif; ?>
--- single.php
+++ single.php
@@ -18,6 +18,8 @@ $right_up_content = get_field("right_up_content", $page_id);
 $left_down_column = get_field("left_down_column", $page_id);
 $right_down_content = $left_down_column;
 $button = get_field("button", $page_id);
+
+$wyswig_content = get_field("content", $page_id);
 ?>
 
 <main id="main" class="main main--subpage">
@@ -28,9 +30,7 @@ $button = get_field("button", $page_id);
         <div class="container">
             <div class="promotion__wrapper">
                 <?php if (!empty(get_post_thumbnail_id($post->ID))): ?>
-                <div class="promotion__image">
-                    <?php echo wp_get_attachment_image(get_post_thumbnail_id($post->ID), 'full', '', [ 'class' => 'object-fit-cover', ]); ?>
-                </div>
+                <div class="promotion__image"><?php echo wp_get_attachment_image(get_post_thumbnail_id($post->ID), 'full', '', [ 'class' => 'object-fit-cover', ]); ?></div>
                 <?php endif; ?>
                 <div class="promotion__content">
                     <h1><?php the_title(); ?></h1>
@@ -47,30 +47,43 @@ $button = get_field("button", $page_id);
         <div class="container">
             <div class="two-box-promo__wrapper">
                 <?php if (!empty($left_column_content)): ?>
-                <div class="two-box-promo__column two-box-promo__column--left">
-                    <?php echo apply_filters('the_title', $left_column_content,); ?>
-                </div>
+                <div class="two-box-promo__column two-box-promo__column--left"><?php echo apply_filters('the_title', $left_column_content); ?></div>
                 <?php endif; ?> <?php if (!empty($right_up_content)): ?>
                 <div class="two-box-promo__column two-box-promo__column--right">
                     <div class="two-box-promo__info">
                         <?php foreach ($right_up_content as $key => $item): ?>
                         <div class="two-box-promo__item">
-                            <div class="two-box-promo__days"><?php echo apply_filters('the_title', $item['days']); ?></div>
-                            <div class="two-box-promo__discount">
-                                <?php echo apply_filters('the_title', $item['discount'],); ?>
+                            <div class="two-box-promo__days">
+                                <?php echo apply_filters('the_title', $item['days'],); ?>
                             </div>
+                            <div class="two-box-promo__discount"><?php echo apply_filters('the_title', $item['discount']); ?></div>
                         </div>
                         <?php endforeach; ?>
                     </div>
                     <div class="two-box-promo__cta">
-                        <div class="two-box-promo__encourage-text"><?php echo apply_filters('the_title', $right_down_content); ?></div>
+                        <div class="two-box-promo__encourage-text">
+                            <?php echo apply_filters('the_title', $right_down_content,); ?>
+                        </div>
                         <?php if (!empty($button)): ?>
-                        <a href="<?php echo esc_html($button['url']); ?>" class="button two-box-promo__button"><?php echo esc_html($button['title']); ?></a>
+                        <a href="<?php echo esc_html($button['url'],); ?>"
+                            class="button two-box-promo__button"
+                        >
+                            <?php echo esc_html($button['title']); ?>
+                        </a>
                         <?php endif; ?>
                     </div>
                 </div>
                 <?php endif; ?>
             </div>
+            <?php if (!empty($wyswig_content)): ?>
+            <div class="wyswig-content mt-4">
+                <div class="container">
+                    <div class="wyswig-content__wrapper">
+                        <?php echo apply_filters('the_title', $wyswig_content,); ?>
+                    </div>
+                </div>
+            </div>
+            <?php endif; ?>
         </div>
     </div>
 
@@ -81,7 +94,9 @@ $button = get_field("button", $page_id);
         <div class="subpage-hero__background subpage-hero__background--plain"></div>
         <div class="container">
             <div class="subpage-hero__wrapper">
-                <h1 class="subpage-hero__title subpage-hero__title--white"><?php echo apply_filters('the_title', 'Blog'); ?></h1>
+                <h1 class="subpage-hero__title subpage-hero__title--white">
+                    <?php echo apply_filters('the_title', 'Blog',); ?>
+                </h1>
             </div>
         </div>
     </div>
@@ -91,9 +106,7 @@ $button = get_field("button", $page_id);
                 <div class="col-12 col-lg-10 offset-lg-1">
                     <div class="single-blog-post__content">
                         <?php if (!empty(get_post_thumbnail_id($post->ID))): ?>
-                        <div class="single-blog-post__image">
-                            <?php echo wp_get_attachment_image(get_post_thumbnail_id($post->ID), 'full', '', ['class' => 'object-fit-cover'],); ?>
-                        </div>
+                        <div class="single-blog-post__image"><?php echo wp_get_attachment_image(get_post_thumbnail_id($post->ID), 'full', '', [ 'class' => 'object-fit-cover', ]); ?></div>
                         <?php endif; ?>
                         <h2><?php the_title(); ?></h2>
                         <p><?php the_content(); ?></p>
